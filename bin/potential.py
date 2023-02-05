63ebc2911c92a12e95faff34aa99b181af358dc1
wei-han <40039920+lp65082001@users.noreply.github.com>
2023-02-05 20:24:13 +0800

    test4

--- bin/potential.py
+++ bin/potential.py
@@ -2,18 +2,30 @@ import numpy as numpy
 
 '''
 class charmm_potential:
-    def __int__(self,x,mode = bonding_only):
-        self.bond_index = x[0]
-        self.bond_atom = x[1]
-        self.angle_index = x[2]
-        self.angle_atom = x[3]
-        self.dihedral_index = x[4]
-        self.dihedral_atom = x[5]
-        self.improper_index = x[6]
-        self.improper_atom = x[7]
-        self.nonbond_index = x[8]
-        self.nonbond_table = x[9]
-        self.position = x[10]
+    def __int__(self,x,,ymode = bonding_only):
+        if (mode == bonding_only):
+            self.bond_index = x[0]
+            self.bond_atom = x[1]
+            self.angle_index = x[2]
+            self.angle_atom = x[3]
+            self.dihedral_index = x[4]
+            self.dihedral_atom = x[5]
+            self.improper_index = x[6]
+            self.improper_atom = x[7]
+            self.nonbond_index = x[8]
+            self.nonbond_table = x[9]
+            self.position = x[10]
+            self.structure = y
+        else:
+            print('Error XD')
+
+    #def parameter_table(self):
+        
+        
+    def bond_parameter_table(self):
+        for i in range(0,self.bond_index.shape[0]):
+            self.structure.bond_types[(self.bond_atom[i,0],self.bond_atom[i,1])]
+
 
 '''
 
