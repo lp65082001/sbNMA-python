0415cfefce546036905788287f0ddba9518d675c
wei-han <40039920+lp65082001@users.noreply.github.com>
2023-02-05 19:09:41 +0800

    test3

--- bin/potential.py
+++ bin/potential.py
@@ -2,5 +2,18 @@ import numpy as numpy
 
 '''
 class charmm_potential:
+    def __int__(self,x,mode = bonding_only):
+        self.bond_index = x[0]
+        self.bond_atom = x[1]
+        self.angle_index = x[2]
+        self.angle_atom = x[3]
+        self.dihedral_index = x[4]
+        self.dihedral_atom = x[5]
+        self.improper_index = x[6]
+        self.improper_atom = x[7]
+        self.nonbond_index = x[8]
+        self.nonbond_table = x[9]
+        self.position = x[10]
+
 '''
 
