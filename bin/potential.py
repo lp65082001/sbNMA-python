550999232504f3e5d8d948af59da55c28a9d30f7
wei-han <40039920+lp65082001@users.noreply.github.com>
2023-02-07 21:05:41 +0800

    20230207_par_modify

--- bin/potential.py
+++ bin/potential.py
@@ -11,18 +11,17 @@ class charmm_potential:
             self.bond_index = x[0]
             self.angle_index = x[1]
             self.dihedral_index = x[2]
-            self.improper_index = x[3]
-            self.nonbond_index = x[4]
-            self.nonbond_table = x[5]
-            self.position = x[6]
-            self.type = x[7]
+            self.nonbond_index = x[3]
+            self.nonbond_table = x[4]
+            self.position = x[5]
+            self.type = x[6]
             self.structure = y
         else:
             print('Error XD')
 
-    #def parameter_table(self):
-        
-        
+    def parameter_table(self):
+        return [self.bond_parameter_table(),self.angle_parameter_table(),self.dihedral_parameter_table()]
+            
     def bond_parameter_table(self):
         bond_par = []
         for i in range(0,self.bond_index.shape[0]):
@@ -33,7 +32,6 @@ class charmm_potential:
     
     def angle_parameter_table(self):
         angle_par = []
-        #print(self.params.angle_types[('OH1', 'CP2','CP3' )])
         for i in range(0,self.angle_index.shape[0]):
             angle_par.append([self.params.angle_types[(self.type[self.angle_index[i,0]], self.type[self.angle_index[i,1]], self.type[self.angle_index[i,2]])].k,
             self.params.angle_types[(self.type[self.angle_index[i,0]], self.type[self.angle_index[i,1]], self.type[self.angle_index[i,2]])].theteq])
@@ -42,10 +40,10 @@ class charmm_potential:
     
     def dihedral_parameter_table(self):
         dihedral_par = []
-        #print(self.params.angle_types[('OH1', 'CP2','CP3' )])
         for i in range(0,self.dihedral_index.shape[0]):
-            dihedral_par.append(self.params.dihedral_types[(self.type[self.dihedral_index[i,0]], self.type[self.dihedral_index[i,1]], self.type[self.dihedral_index[i,2]], self.type[self.dihedral_index[i,3]])])
-
-        return np.array(dihedral_par)
+            dihedral_par.append([self.params.dihedral_types[(self.type[self.dihedral_index[i,0]], self.type[self.dihedral_index[i,1]], self.type[self.dihedral_index[i,2]], self.type[self.dihedral_index[i,3]])][0].phi_k,
+            self.params.dihedral_types[(self.type[self.dihedral_index[i,0]], self.type[self.dihedral_index[i,1]], self.type[self.dihedral_index[i,2]], self.type[self.dihedral_index[i,3]])][0].phase])
+        
+        return np.array(dihedral_par).reshape(-1,2)
 
 
